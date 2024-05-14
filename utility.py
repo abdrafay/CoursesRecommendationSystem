09d0e69fa59067c84973291b9b9c7d9d827bd2d9
abdrafay <zainrafay12@gmail.com>
2024-05-15 02:08:26 +0500

    Project Init

---
+++ utility.py
@@ -0,0 +1,43 @@
+Computing_Core_Courses={
+    "Course Name":["Programming Fundamentals","Object Oriented Programming",
+                   "Data Structures & Algorithms","Discrete Structures",
+                   "Operating Systems","Database Systems","Software Engineering",
+                   "Computer Networks","Information Security","Final Year Project I",
+                   "Final Year Project"]
+}
+
+General_Education_Courses={
+    "Course Name":["English Composition & Comprehension","Technical & Business Writing",
+                   "Communication & Presentation Skills","Professional Practices","Intro to Info. & Comm. Technologies","Pakistan Studies",
+                   "Islamic Studies/ Ethics"]
+    
+}
+
+University_Elective_Courses={
+    "Course Name":["Foreign Language","Social Service","Management Related",
+                "Social Science Related","Economy Related"]
+                }
+
+Mathematics_and_Science_Foundation_Courses={
+    "Course Name":["Calculus & Analytical Geometry",
+                 "Probability & Statistics","Linear Algebra","Applied Physics"]
+}
+
+Computer_Science_CORE_Compulsory_courses={
+    "Course Name":["Compiler Construction" ,"Comp. Organization & Assembly Language" ,
+        "Digital Logic Design" ,"Design & Analysis of Algorithms" ,
+        "Parallel & Distributed Computing" ,"Artificial Intelligence" ,
+        "Theory of Automata"]
+}
+
+Computer_Science_SUPPORTING_courses={
+    "Course Name":["Differential Equations","Multi-variate Calculus",
+            "Graph Theory","Theory of Programming Languages","Numerical Computing"
+    ]
+}
+
+Computer_Science_ELECTIVE_courses={
+    "Course Name":["CS Elective – 1" ,"CS Elective – 2" ,"CS Elective – 3" ,
+            "CS Elective – 4" ,"CS Elective – 5"
+    ]
+}
